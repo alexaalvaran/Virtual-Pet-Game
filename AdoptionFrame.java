32c101121e9cde342b51f362b17542273137df11
Alexa <130896929+alexaalvaran@users.noreply.github.com>
2023-09-30 20:09:07 +1300

    Update AdoptionFrame.java
    changing design to a more mvc design approach

--- AdoptionFrame.java
+++ AdoptionFrame.java
@@ -13,6 +13,7 @@ import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
@@ -21,7 +22,7 @@ import javax.swing.JTextField;
  *
  * @author alei
  */
-public class AdoptionFrame {
+public class AdoptionView {
 
     Toolkit kit = Toolkit.getDefaultToolkit();
     Dimension screenSize = kit.getScreenSize();
@@ -30,84 +31,90 @@ public class AdoptionFrame {
     private int frameWidth = screenWidth;
     private int frameHeight = screenHeight / 2;
     //Get player name
-    private JTextField inputPlayerName;
-    private JPanel playerPanel;
-    private JButton submitPlayerName;
+    private JTextField inputPlayerName = new JTextField(10);;
+    private JPanel playerPanel = new JPanel();
+    private JLabel playerPrompt = new JLabel("Enter your name: ");
+    private JButton submitPlayerName =   submitPlayerName = new JButton("submit");
 
     //Adoption menu 
-    private JFrame adoptionFrame;
-    private JPanel adoptionPanel;
+    private JFrame adoptionFrame = new JFrame("Virtual Pet Game");;
+    private JPanel adoptionPanel =  new JPanel();;
+    JLabel adoptionPrompt = new JLabel("Pick an animal to adopt: ");
     private ButtonGroup pets = new ButtonGroup();
     private JRadioButton newDog = new JRadioButton("Dog", true);
     private JRadioButton newCat = new JRadioButton("Cat");
     private JRadioButton newBird = new JRadioButton("Bird");
 
     //Get animal name
-    private JTextField inputPetName;
-    private JPanel newPetPanel;
-    private JButton submitPetName;
-    
-      //Making the adoption frame
-    public AdoptionFrame() {
-        adoptionFrame = new JFrame("Virtual Pet Game");
+    private JTextField inputPetName  = new JTextField(10);;
+    private JPanel newPetPanel =  new JPanel();
+    JLabel petPrompt = new JLabel("Enter the name of your pet: ");
+    private JButton submitPetName = new JButton("submit");
+
+    //Adoption DB Manager
+    GameDBManager dbManager = new GameDBManager();
+
+    //Making the adoption frame
+    public AdoptionView() {
         adoptionFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         adoptionFrame.setSize(frameWidth, frameHeight);
         adoptionFrame.setResizable(false);
         adoptionFrame.setLocationRelativeTo(null);
-       
+
         getPlayerName();
         adoptionFrame.add(playerPanel);
-        
+
         adoptionMenu();
         adoptionFrame.add(adoptionPanel);
-        
+
         getPetName();
         adoptionFrame.add(newPetPanel);
-        
+
         adoptionFrame.setLayout(new GridLayout(3, 1));
         adoptionFrame.setVisible(true);
     }
-    
+
 //Creating the panel which stores textfield to retrieve player name and button to submit
     public void getPlayerName() {
-        playerPanel = new JPanel();
-        inputPlayerName = new JTextField(10);
-        submitPlayerName = new JButton("submit");
-
-        JLabel playerPrompt = new JLabel("Enter your name: ");
         playerPanel.add(playerPrompt);
         playerPanel.add(inputPlayerName);
         playerPanel.add(submitPlayerName);
+
+        submitPlayerName.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+            boolean found = dbManager.findExistingPlayer(inputPlayerName.getText());
+            if(found == true)
+             {
+            JOptionPane.showMessageDialog(null, "Existing player already exists", "VirtualPetApp", JOptionPane.INFORMATION_MESSAGE);
+            }
+            dbManager.newPlayer(inputPlayerName.getText());
+            }
+        });
+
     }
 
     //Creating the adoption menu : buttons
     public void adoptionMenu() {
-        adoptionPanel = new JPanel();
-
+    
         //Adding buttons to button group (radio buttons)
         pets.add(newDog);
         pets.add(newCat);
         pets.add(newBird);
-
-        JLabel adoptionPrompt = new JLabel("Pick an animal to adopt: ");
 
         //Adding buttons to panel
         adoptionPanel.add(adoptionPrompt);
         adoptionPanel.add(newDog);
         adoptionPanel.add(newCat);
         adoptionPanel.add(newBird);
-       
 
     }
 
- 
     //Creating the panel that has textfield which retrieves the name of the pet and submit button
     public void getPetName() {
-        newPetPanel = new JPanel();
-        inputPetName = new JTextField(10);
-        submitPetName = new JButton("submit");
-
-        JLabel petPrompt = new JLabel("Enter the name of your pet: ");
+      
         newPetPanel.add(petPrompt);
         newPetPanel.add(inputPetName);
         newPetPanel.add(submitPetName);
@@ -116,6 +123,7 @@ public class AdoptionFrame {
         submitPetName.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                dbManager.newPet(inputPetName.getText());
                 adoptionFrame.dispose();
                 MainFrame mainFrame = new MainFrame();
             }
@@ -161,7 +169,5 @@ public class AdoptionFrame {
     public void setSubmitPlayerName(JButton submitPlayerName) {
         this.submitPlayerName = submitPlayerName;
     }
-    
-    
 
 }
