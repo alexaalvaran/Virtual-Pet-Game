559bbb3f80a80ba1433bfe5b03c9baeb4915ddb2
Alexa <130896929+alexaalvaran@users.noreply.github.com>
2023-09-30 20:07:43 +1300

    Add files via upload
    Game database manager - connects to embedded database which has a table called SavedGames which has two player names upon startup for demonstration purposes; Checks if player has existing record upon name submission and saves any new player and pet names

---
+++ PetGameDBManager.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author alei
+ */
+public class PetGameDBManager 
+{
+    private static final String USER_NAME = "group44"; //Database username
+    private static final String PASSWORD = "44"; //Database password
+    private static final String URL = "jdbc:derby:PetGameDB_Ebd; create = true"; //url of the DB host
+    
+    private Connection conn;
+    
+    public PetGameDBManager()
+    {
+        establishConnection();
+    }
+    
+    public Connection getConnection()
+    {
+        return this.conn;
+    }
+    
+    public void establishConnection()
+    {
+        if(this.conn == null)
+        {
+            try
+            {
+                conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+                System.out.println(URL + "Get connected successfully...");
+                
+            }
+            catch(SQLException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    
+    public void closeConnections()
+    {
+        if(conn != null)
+        {
+            try
+            {
+                conn.close();
+            }
+            catch(SQLException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
