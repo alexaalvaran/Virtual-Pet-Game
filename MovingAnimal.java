82aff3719ff1dc3da484498b8c60bac1f08f6166
Alexa <130896929+alexaalvaran@users.noreply.github.com>
2023-10-26 15:12:51 +1300

    Update MovingAnimal.java

--- MovingAnimal.java
+++ MovingAnimal.java
@@ -8,8 +8,6 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.ImageIcon;
